6169ff148ee49ff21bfb184f87ffd1f24b5b5220
Fedir Katushonok <fedir.katushonok@gmail.com>
2024-03-05 22:52:10 +0200

    Implemented changes required for assignment-4 part 2 to work

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFDIR="/etc/finder-app"
+username=$(cat ${CONFDIR}/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONFDIR}/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -59,6 +60,8 @@ do
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+
+echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
