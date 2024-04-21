2d2b25a6c36577f09ea3796f86115725fe7e9182
CeSiumUA <fedir.katushonok@gmail.com>
2024-04-21 13:07:05 +0300

    Added printk to debug

--- aesd-char-driver/aesd-circular-buffer.h
+++ aesd-char-driver/aesd-circular-buffer.h
@@ -18,7 +18,7 @@
 #endif
 
 #ifdef __KERNEL__
-#define DEBUG_LOG(fmt, args...)
+#define DEBUG_LOG(fmt, args...)                            printk(KERN_DEBUG fmt, ## args)
 #else
 #define DEBUG_LOG(fmt, args...)                            printf(fmt, ## args)
 #endif
